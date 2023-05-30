f5bac828e09bca2655e9b5406f4082cccdf2b9cd
Liza <amzalliza.amzalliza@gmail.com>
2023-05-30 23:38:04 +0200

    add get

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-//les imports 
 var bodyParser = require('body-parser');
 var express = require('express');
 var apiRouter = require('./apiRouter').router;
+var db = require('./db'); // Importez votre module de gestion de la base de données
 
 // Instancier 
 var server = express();
@@ -12,14 +12,27 @@ server.use(bodyParser.json());
 
 //configure routes 
 server.get('/', function(req, res) {
-    res.setHeader('Content-Type', 'text/html');
-    res.status(200).send('<h1>Helllllllllllllllo</h1>')
-
+  res.setHeader('Content-Type', 'text/html');
+  res.status(200).send('<h1>Hello</h1>');
 });
 
-server.use('/api/', apiRouter);
-//launch server
+// Endpoint GET pour récupérer les données de la base de données
+server.get('/api/data', function(req, res) {
+  // Utilisez votre module de gestion de la base de données pour récupérer les données
+  db.getData(function(err, data) {
+    if (err) {
+      console.log(err);
+      res.status(500).send('Une erreur est survenue lors de la récupération des données');
+    } else {
+      res.status(200).json(data); // Renvoyer les données en format JSON
+    }
+  });
+});
 
+// Utilisez le router pour les autres routes
+server.use('/api/', apiRouter);
+
+//launch server
 server.listen(8080, function(){
-    console.log('Server en écoute');
-})
+  console.log('Server en écoute');
+});
