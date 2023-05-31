0f7a975096464504457d675ab4c5a5701d05b951
Liza <amzalliza.amzalliza@gmail.com>
2023-05-31 22:03:29 +0200

    add get

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var bodyParser = require('body-parser');
 var express = require('express');
 var apiRouter = require('./apiRouter').router;
+var mysql = require('mysql');
 
 // Instancier 
 var server = express();
@@ -11,14 +12,40 @@ server.use(bodyParser.json());
 
 //configure routes 
 server.get('/', function(req, res) {
-    res.setHeader('Content-Type', 'text/html');
-    res.status(200).send('<h1>Helllllllllllllllo</h1>')
-
+  res.setHeader('Content-Type', 'text/html');
+  res.status(200).send('<h1>Hello</h1>');
 });
 
-server.use('/api/', apiRouter);
-//launch server
+// Endpoint GET pour récupérer les données de la base de données
+server.get('/api/data', function(req, res) {
+  // Configuration de la connexion à la base de données
+  var connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'test',
+    database: 'database_development_retro'
+  });
 
+  // Établissement de la connexion à la base de données
+  connection.connect();
+
+  connection.query('SELECT * FROM USERS', function(error, results, fields) {
+    if (error) {
+      console.log(error);
+      res.status(500).send('Une erreur est survenue lors de la récupération des données');
+    } else {
+      res.status(200).json(results); 
+    }
+  });
+
+  // Fermeture de la connexion à la base de données
+  connection.end();
+});
+
+// Utilisez le router pour les autres routes
+server.use('/api/', apiRouter);
+
+//launch server
 server.listen(8080, function(){
-    console.log('Server en écoute');
-})
+  console.log('Server en écoute');
+});
